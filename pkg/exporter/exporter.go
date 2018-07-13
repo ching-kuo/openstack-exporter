02c75162daa4af7b70c509a931d08b94ceeaf5c8
Gene Kuo <igene@igene.tw>
2018-07-13 16:01:19 +0000

    Fix Indent

--- pkg/exporter/exporter.go
+++ pkg/exporter/exporter.go
@@ -25,7 +25,7 @@ func NewOpenStackExporter(provider *gophercloud.ProviderClient, region string) *
 		collectors: []prometheus.Collector{
 			collector.NewComputeCollector(provider, region),
 			collector.NewBlockStorageCollector(provider, region),
-			collector.NewNetworkCollector(provider, region),
+	        collector.NewNetworkCollector(provider, region),
             collector.NewClusterHealthCollector(provider, region),
 		},
 	}
